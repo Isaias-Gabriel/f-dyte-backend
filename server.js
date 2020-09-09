67da7a08e6511cca83d14dda1fe0859805743170
= <=>
2020-09-09 10:38:37 -0300

    commit

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, {
-//     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
-// });
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
+});
 
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log('MongoDB database connection established successfully');
-// })
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+})
 
 const evaluatorRouter = require('./routes/evaluator');
 
