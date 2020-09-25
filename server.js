9dc39f10f659c8fd83d0a4ff23788ed992b191ec
= <=>
2020-09-25 10:54:45 -0300

    commit

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const port = process.env.PORT || 5000;
 // server.listen(webSocketsServerPort);
 //console.log('Listening on port 8000');
 
-app.use(cors({
-    origin: '*',//['https://www.fdytte.com', 'http://192.168.1.5:3000'],
-    allowedHeaders: '*', //['Content-Type', 'Authorization'],
-}));
+app.use(cors());
+
+// {
+//     origin: '*',//['https://www.fdytte.com', 'http://192.168.1.5:3000'],
+//     allowedHeaders: '*', //['Content-Type', 'Authorization'],
+// }
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     next();
